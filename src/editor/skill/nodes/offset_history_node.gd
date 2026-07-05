638c3ad39329004225fc0b93730c663cf0d0180a
LC_022 <101245833+sblzdddd@users.noreply.github.com>
2026-07-06 03:05:55 +0800

    fix currentColor error, update nodes

--- src/editor/skill/nodes/offset_history_node.gd
+++ src/editor/skill/nodes/offset_history_node.gd
@@ -8,8 +8,9 @@ func info() -> void:
 	node_category = SkillNodeCategory.Category.CARD_HOLDER
 	node_name = "按历史顺序获取玩家"
 	input_slot_specs = [
-		SkillInputSpec.create(SkillConstants.array_type(PortType.CARD_HOLDER), "当前玩家"),
+		SkillInputSpec.create(PortType.CARD_HOLDER, "当前玩家"),
 		SkillInputSpec.from_widget("偏移数量", SkillInputNumber.create(-5.0, 5.0, 1.0, 1.0, true)),
+		SkillInputSpec.from_widget("玩家数量", SkillInputNumber.create(0.0, 5.0, 1.0, 1.0, true)),
 	]
 	output_slot_specs = [
 		SkillSlotSpec.create(SkillConstants.array_type(PortType.CARD_HOLDER), "玩家"),
--- src/editor/skill/nodes/offset_player_node.gd
+++ src/editor/skill/nodes/offset_player_node.gd
@@ -10,6 +10,7 @@ func info() -> void:
 	input_slot_specs = [
 		SkillInputSpec.create(PortType.CARD_HOLDER, "当前玩家"),
 		SkillInputSpec.from_widget("偏移数量", SkillInputNumber.create(-5.0, 5.0, 1.0, 1.0, true)),
+		SkillInputSpec.from_widget("玩家数量", SkillInputNumber.create(0.0, 5.0, 1.0, 1.0, true)),
 	]
 	output_slot_specs = [
 		SkillSlotSpec.create(SkillConstants.array_type(PortType.CARD_HOLDER), "玩家"),
