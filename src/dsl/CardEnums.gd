769aac06c44b4c63691862e81118b3bec9b3cdb9
LC_022 <101245833+sblzdddd@users.noreply.github.com>
2026-07-09 01:07:46 +0800

    add editor icons for card suits and values

--- src/dsl/CardEnums.gd
+++ src/dsl/CardEnums.gd
@@ -5,26 +5,49 @@ enum Suit {
 	DIAMONDS,
 	HEARTS,
 	SPADES,
+	JOKERS,
 }
 
 enum Rank {
 	NONE,
-	ACE,
-	TWO,
-	THREE,
-	FOUR,
-	FIVE,
-	SIX,
-	SEVEN,
-	EIGHT,
-	NINE,
-	TEN,
-	JACK,
-	QUEEN,
-	KING,
+	THREE = 3,
+	FOUR = 4,
+	FIVE = 5,
+	SIX = 6,
+	SEVEN = 7,
+	EIGHT = 8,
+	NINE = 9,
+	TEN = 10,
+	JACK = 11,
+	QUEEN = 12,
+	KING = 13,
+	ACE = 21,
+	TWO = 22,
+	WILD = 23,
+	SMALL_JOKER = 30,
+	LARGE_JOKER = 40,
 }
 
 enum Type {
 	NORMAL,
 	SKILL
 }
+
+static func to_normal(rank: Rank) -> int:
+	match rank:
+		Rank.ACE: return 14
+		Rank.TWO: return 15
+		_: return int(rank)
+
+static func to_low(rank: Rank) -> int:
+	match rank:
+		Rank.ACE: return 1
+		Rank.TWO: return 2
+		_: return int(rank)
+
+static func to_wrap_high(rank: Rank) -> int:
+	match rank:
+		Rank.JACK, Rank.QUEEN, Rank.KING: return 10
+		Rank.ACE: return 14
+		Rank.TWO: return 15
+		_: return int(rank)
